467737c28a8ab3e5208332c624b461aa93fe7e56
Eduard Gaman <eduard.gaman@kontron.ro>
2024-04-02 15:35:37 +0300

    local testing

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const apiService = axios.create({
-  // baseURL: 'http://localhost:8080/api/v1',
-  baseURL: 'https://sub-tracker-backend-257a758d05ec.herokuapp.com/api/v1',
+  baseURL: 'http://localhost:8080/api/v1',
+  // baseURL: 'https://sub-tracker-backend-257a758d05ec.herokuapp.com/api/v1',
   withCredentials: true,
 });
 
